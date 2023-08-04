ba795c792d5962e3dd757fa9c57dbeff028082c7
ABHIJITH P SHABU <96898946+A-B-H-I-2022@users.noreply.github.com>
2023-08-04 12:14:52 +0530

    Time: 61 ms (95.49%) | Memory: 13.3 MB (93.58%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        r = collections.defaultdict(set)
+        c = collections.defaultdict(set)
+        part = collections.defaultdict(set)
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == ".":
+                    continue
+                elif(board[i][j] in r[i] or board[i][j] in c[j] or board[i][j] in part[(i//3,j//3)]):
+                    return False
+                else:
+                    r[i].add(board[i][j])
+                    c[j].add(board[i][j])
+                    part[(i//3,j//3)].add(board[i][j])
+        return True
