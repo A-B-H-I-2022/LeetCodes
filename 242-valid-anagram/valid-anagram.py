bdc7509e6d00bb3ab781c81da5ff238a112588ec
ABHIJITH P SHABU <96898946+A-B-H-I-2022@users.noreply.github.com>
2023-08-11 10:13:49 +0530

    Time: 27 ms (92.49%) | Memory: 15.1 MB (29.24%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -5,9 +5,21 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        a = Counter(s)
-        b = Counter(t)
-        if a == b:
-            return True
-        else:
+        # a = Counter(s)
+        # b = Counter(t)
+        # if a == b:
+        #     return True
+        # else:
+        #     return False
+
+        if len(t) != len(s):
             return False
+        freq = [0]*26
+        for i in range(len(s)):
+            freq[ord(s[i]) - ord('a')] += 1
+            freq[ord(t[i]) - ord('a')] -= 1
+        for i in freq:
+            if i != 0:
+                return False
+        return True
+            
