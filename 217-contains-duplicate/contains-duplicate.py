1a263e2df933e646601e9d6d8133cef7f31c18bd
ABHIJITH P SHABU <96898946+A-B-H-I-2022@users.noreply.github.com>
2023-08-11 10:01:34 +0530

    Time: 423 ms (93.30%) | Memory: 29.2 MB (36.21%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,9 +1,42 @@
 class Solution(object):
     def containsDuplicate(self, nums):
-        a = list(set(nums))
-        l = len(a)
-        m = len(nums)
-        if l < m:
+        answer = len(list(set(nums)))
+        ans = len(nums)
+        if answer < ans:
             return True
         else:
             return False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # a = list(set(nums))
+        # l = len(a)
+        # m = len(nums)
+        # if l < m:
+        #     return True
+        # else:
+        #     return False
