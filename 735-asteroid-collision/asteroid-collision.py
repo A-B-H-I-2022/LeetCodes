19ef631e28da396d4110f2f198dc2a8ab343cc2a
ABHIJITH P SHABU <96898946+A-B-H-I-2022@users.noreply.github.com>
2023-08-19 16:50:00 +0530

    Time: 65 ms (91.68%) | Memory: 14.5 MB (29.44%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -5,7 +5,8 @@ class Solution(object):
         :rtype: List[int]
         """
         stack = []
-        i,l,top = 0,len(asteroids),0
+        i,top = 0,0
+        l = len(asteroids)
         while i < l:
             if asteroids[i] > 0:
                 stack.append(asteroids[i])
@@ -27,3 +28,50 @@ class Solution(object):
                         stack.pop()
             i += 1
         return stack
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # stack = []
+        # i,l,top = 0,len(asteroids),0
+        # while i < l:
+        #     if asteroids[i] > 0:
+        #         stack.append(asteroids[i])
+        #     else:
+        #         flag = 1
+        #         while flag:
+        #             if stack and stack[-1] > 0:
+        #                 top = stack[-1]
+        #             else:
+        #                 stack.append(asteroids[i])
+        #                 flag = 0
+        #                 break
+        #             if top > abs(asteroids[i]):
+        #                 flag = 0
+        #             elif top == abs(asteroids[i]):
+        #                 stack.pop()
+        #                 flag = 0
+        #             else:
+        #                 stack.pop()
+        #     i += 1
+        # return stack
