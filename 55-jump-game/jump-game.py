a30805a9ab7b382008c9cf14b569ba84431503bc
ABHIJITH P SHABU <96898946+A-B-H-I-2022@users.noreply.github.com>
2023-08-19 11:51:46 +0530

    Time: 379 ms (70.57%) | Memory: 14.7 MB (10.42%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -4,20 +4,44 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        current = nums[0]
-        for i in range(1,len(nums)):
-            if current == 0:
+        initial = nums[0]
+        for i in range(1, len(nums)):
+            if initial == 0:
                 return False
-            current -= 1
-            current = max(current, nums[i])
+            initial -= 1
+            initial = max(initial, nums[i])
         return True
-        # left,right,jump = 0,0,0
-        # l = len(nums)
-        # while right <= l - 1:
-        #     for i in range(left,right+1):
-        #         jump = max(jump,i + nums[i])
-        #     left = right + 1
-        #     right = jump
-        #     if right == l-1:
-        #         return True
-        # return False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # current = nums[0]
+        # for i in range(1,len(nums)):
+        #     if current == 0:
+        #         return False
+        #     current -= 1
+        #     current = max(current, nums[i])
+        # return True
+    
