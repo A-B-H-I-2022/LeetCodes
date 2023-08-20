eeaa7751f8a43d4ab70b22a8d551f929cacbde73
ABHIJITH P SHABU <96898946+A-B-H-I-2022@users.noreply.github.com>
2023-08-20 08:13:14 +0530

    Time: 502 ms (91.21%) | Memory: 23.5 MB (26.83%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
@@ -5,16 +5,27 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        nums.sort()
-        answer,left,right = 0,0,len(nums)-1
-        while left < right:
-            summ = nums[left] + nums[right]
-            if summ > k:
-                right -= 1
-            elif summ < k:
-                left += 1
-            else:
+        map = {}
+        answer = 0
+        for i in nums:
+            if k-i in map and map[k-i]>0:
                 answer += 1
-                left += 1
-                right -= 1
+                map[k-i] -= 1
+            elif i not in map:
+                map[i] = 1
+            else:
+                map[i] += 1
         return answer
+        # nums.sort()
+        # answer,left,right = 0,0,len(nums)-1
+        # while left < right:
+        #     summ = nums[left] + nums[right]
+        #     if summ > k:
+        #         right -= 1
+        #     elif summ < k:
+        #         left += 1
+        #     else:
+        #         answer += 1
+        #         left += 1
+        #         right -= 1
+        # return answer
