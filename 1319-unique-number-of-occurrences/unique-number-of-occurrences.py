58d27a4e226d3deb8053bf7f91f6eb932a83e2ae
ABHIJITH P SHABU <96898946+A-B-H-I-2022@users.noreply.github.com>
2023-08-22 21:15:19 +0530

    Time: 26 ms (32.79%) | Memory: 13.5 MB (12.46%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
@@ -4,22 +4,20 @@ class Solution(object):
         :type arr: List[int]
         :rtype: bool
         """
-        map = {}
-        l = []
-        for i in arr:
-            if i in map:
-                map[i] += 1
-            else:
-                map[i] = 1
-
-        for v in map.values():
-            l.append(v)
+        value = [v for v in Counter(arr).values()]
+        return len(value) == len(set(value))
+        # map = {}
+        # l = []
+        # for i in arr:
+        #     if i in map:
+        #         map[i] += 1
+        #     else:
+        #         map[i] = 1
+        # for v in map.values():
+        #     l.append(v)
         
-        if len(l) != len(set(l)):
-            return False
-        else:return True
-            # if count(v) > 1:
-            #     return False
-            # else:
-            #     return True
- 
+        # if len(l) != len(set(l)):
+        #     return False
+        # else:
+        #     return True
+           
