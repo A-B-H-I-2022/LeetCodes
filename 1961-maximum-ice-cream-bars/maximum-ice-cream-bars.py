a09ef1d81c866c62f43b12e6a0e6d24c5ef13fb9
ABHIJITH P SHABU <96898946+A-B-H-I-2022@users.noreply.github.com>
2023-08-23 22:21:33 +0530

    Time: 665 ms (89.87%) | Memory: 25 MB (73.42%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.py
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.py
@@ -5,16 +5,22 @@ class Solution(object):
         :type coins: int
         :rtype: int
         """
-        
-        answer,count = 0,0
-        ans = []
-        if sum(costs) <= coins:
-            return len(costs)
-        else:
-            costs.sort()
-            for i in costs:
-                answer += i
-                if answer <= coins:
-                    count += 1
-                else:break
-        return count
+        costs.sort()
+        l = len(costs)
+        for i in range(l):
+            coins -= costs[i]
+            if coins < 0:
+                return i
+        return l
+        # answer,count = 0,0
+        # ans = []
+        # if sum(costs) <= coins:
+        #     return len(costs)
+        # else:
+        #     costs.sort()
+        #     for i in costs:
+        #         answer += i
+        #         if answer <= coins:
+        #             count += 1
+        #         else:break
+        # return count
