f0bebd660d190befa8a62772b4388e5031b84214
ABHIJITH P SHABU <96898946+A-B-H-I-2022@users.noreply.github.com>
2023-08-25 21:26:32 +0530

    Time: 1174 ms (20.70%) | Memory: 18.7 MB (94.08%) - LeetSync

---
+++ 134-gas-station/gas-station.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def canCompleteCircuit(self, gas, cost):
+        """
+        :type gas: List[int]
+        :type cost: List[int]
+        :rtype: int
+        """
+        start = 0
+        end = 0 
+        sum = 0
+        length = 0
+        while start<len(gas) and length < len(gas):
+            sum += gas[end]-cost[end]
+            length +=1
+            while start<len(gas) and sum < 0:
+                sum -= gas[start] - cost[start]
+                start +=1
+                length-=1
+            end+=1
+            end = end % len(gas)
+        if length == len(gas):
+            return start
+        return -1
+        
