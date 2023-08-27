92995b026c130d70173f298a92d87637db0ad362
ABHIJITH P SHABU <96898946+A-B-H-I-2022@users.noreply.github.com>
2023-08-27 10:06:54 +0530

    Time: 1001 ms (74.61%) | Memory: 19.8 MB (61.48%) - LeetSync

--- 134-gas-station/gas-station.py
+++ 134-gas-station/gas-station.py
@@ -5,20 +5,54 @@ class Solution(object):
         :type cost: List[int]
         :rtype: int
         """
-        start = 0
-        end = 0 
-        sum = 0
-        length = 0
-        while start<len(gas) and length < len(gas):
-            sum += gas[end]-cost[end]
-            length +=1
-            while start<len(gas) and sum < 0:
-                sum -= gas[start] - cost[start]
-                start +=1
-                length-=1
-            end+=1
-            end = end % len(gas)
-        if length == len(gas):
-            return start
-        return -1
+        totalgain = 0
+        currentgain = 0
+        answer = 0
+        for i in range(len(gas)):
+            totalgain += gas[i] - cost[i]
+            currentgain += gas[i] - cost[i]
+            if currentgain < 0:
+                currentgain = 0
+                answer = i + 1
+        return answer if totalgain >= 0 else -1
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        # start = 0
+        # end = 0 
+        # sum = 0
+        # length = 0
+        # while start<len(gas) and length < len(gas):
+        #     sum += gas[end]-cost[end]
+        #     length +=1
+        #     while start<len(gas) and sum < 0:
+        #         sum -= gas[start] - cost[start]
+        #         start +=1
+        #         length-=1
+        #     end+=1
+        #     end = end % len(gas)
+        # if length == len(gas):
+        #     return start
+        # return -1
         
