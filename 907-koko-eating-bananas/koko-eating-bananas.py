1399ce651797112d24e78fc28f23e2e52f710446
ABHIJITH P SHABU <96898946+A-B-H-I-2022@users.noreply.github.com>
2023-08-29 22:53:30 +0530

    Time: 254 ms (95.03%) | Memory: 14.5 MB (72.29%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -6,29 +6,15 @@ class Solution(object):
         :rtype: int
         # """
         # print(math.ceil(4/3))
-        # l,r = 1,max(piles) 
-        # a = 0
-        # while l < r:
-        #     k = (l+r)//2
-        #     hours = 0
-        #     for i in piles:
-        #         hours += math.ceil(i / k)+1
-        #     if hours <= h:
-        #         a = k
-        #         r = k
-        #     else:
-        #         l = k + 1
-        # return l
-
-        l=1
-        r=max(piles)
-        while l<r:
-            mid=(l+r)//2
-            c=0
-            for j in piles:
-                c+=((j-1)//mid)+1
-            if c>h:
-                l=mid+1
+        l,r = 1,max(piles) 
+        a = 0
+        while l < r:
+            k = (l+r)//2
+            hours = 0
+            for i in piles:
+                hours += ((i-1)//k)+1
+            if hours > h:
+                  l = k + 1
             else:
-                r=mid
+                r = k
         return l
