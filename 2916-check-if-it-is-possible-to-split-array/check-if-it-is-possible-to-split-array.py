25268fc594d085f72dffe4fa471f2fe586030a50
ABHIJITH P SHABU <96898946+A-B-H-I-2022@users.noreply.github.com>
2023-09-02 19:51:15 +0530

    Time: 26 ms (84.00%) | Memory: 13.3 MB (36.36%) - LeetSync

--- 2916-check-if-it-is-possible-to-split-array/check-if-it-is-possible-to-split-array.py
+++ 2916-check-if-it-is-possible-to-split-array/check-if-it-is-possible-to-split-array.py
@@ -10,39 +10,5 @@ class Solution(object):
         flag = 0
         for i in range(len(nums)-1):
             if nums[i] + nums[i+1] >= m:
-                flag = 1
-        return True if flag else False
-
-
-        # l = 0
-        # flag = 1
-        # r = len(nums)-1
-        # if len(nums) <= 2:
-        #     return True
-        # while l < r:
-        #     if sum(nums[l:r+1]) >= m:
-        #         r -= 1
-        #     else:
-        #         r += 1
-        #         l += 1
-        #         if sum(nums[l:r+1]) < m:
-        #             flag = 0
-        #             break
-        # if flag == 0:
-        #     l = 0
-        #     r = len(nums) - 1
-        #     while l < r:
-        #         if sum(nums[l:r+1]) >= m:
-        #             l += 1
-        #         else:
-        #             r -= 1
-        #             l -= 1
-        #             if sum(nums[l:r+1]) < m:
-        #                 flag = 0
-        #                 break
-
-
-        # if flag or l == r:
-        #     return True
-        # else:
-        #     return False
+                return True
+        return False
