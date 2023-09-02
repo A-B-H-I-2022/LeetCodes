0542d656130de8985dfe9cc275a52ede6e3ff77a
ABHIJITH P SHABU <96898946+A-B-H-I-2022@users.noreply.github.com>
2023-09-02 18:39:12 +0530

    Time: 577 ms (80.46%) | Memory: 31.4 MB (43.68%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -5,21 +5,43 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        count = collections.Counter()
-        ans,l = 0,0
-        res = 0
-        n = len(nums)
-        for i in range(n):
-            ans += nums[i]
-            if count[nums[i]] == 0:
-                l += 1
-            count[nums[i]] += 1
-            if i >= k:
-                ans -= nums[i-k]
-                count[nums[i-k]] -= 1
-                if count[nums[i-k]] == 0:
-                    l -= 1
-            if i >= k-1:
-                if l == k:
-                    res = max(res, ans)
-        return res
+        max_sum = 0
+        current_sum = 0
+        left,right = 0,0
+        taken = set()
+        while right < len(nums):
+            if nums[right] not in taken:
+                current_sum += nums[right]
+                taken.add(nums[right])
+                right += 1
+
+                if right - left == k:
+                    max_sum = max(max_sum , current_sum)
+                    current_sum -= nums[left]
+                    taken.remove(nums[left])
+                    left += 1
+            
+            else:
+                current_sum -= nums[left]
+                taken.remove(nums[left])
+                left += 1
+
+        return max_sum
+        # count = collections.Counter()
+        # ans,l = 0,0
+        # res = 0
+        # n = len(nums)
+        # for i in range(n):
+        #     ans += nums[i]
+        #     if count[nums[i]] == 0:
+        #         l += 1
+        #     count[nums[i]] += 1
+        #     if i >= k:
+        #         ans -= nums[i-k]
+        #         count[nums[i-k]] -= 1
+        #         if count[nums[i-k]] == 0:
+        #             l -= 1
+        #     if i >= k-1:
+        #         if l == k:
+        #             res = max(res, ans)
+        # return res
