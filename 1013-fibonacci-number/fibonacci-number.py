37f625e5dcad8bc2070b78ad04ddc88468199db3
ABHIJITH P SHABU <96898946+A-B-H-I-2022@users.noreply.github.com>
2023-09-06 16:46:28 +0530

    Time: 24 ms (44.77%) | Memory: 13.2 MB (48.55%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -4,22 +4,11 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        # dp = [-1]*(n+1)
-        # def fibonacci(n):
-        #     if dp[n] != -1:
-        #         return dp[n]
-        #     dp[n] = dp[n-1] + dp[n-2]
-        # return dp[n]
-        # a,b,s = 0,0,0
-    
-        a,b,s = 0, 1, 0
-        if n>1:
-            for i in range(1,n):
-                s = a+b
-                a = b
-                b = s 
-            return s
-        elif n == 1:
-            return 1
-        else:
-            return 0
+        if n<=1:
+            return n
+        prev2,prev=0,1
+        for i in range(2,n+1):
+            curr=prev+prev2
+            prev2=prev
+            prev=curr
+        return curr
