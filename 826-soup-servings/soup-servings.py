1235871274527e0300549588bbf1e8b214620d65
ABHIJITH P SHABU <96898946+A-B-H-I-2022@users.noreply.github.com>
2023-09-07 11:09:08 +0530

    Time: 32 ms (19.90%) | Memory: 15.6 MB (12.78%) - LeetSync

--- 826-soup-servings/soup-servings.py
+++ 826-soup-servings/soup-servings.py
@@ -4,21 +4,7 @@ class Solution(object):
         :type n: int
         :rtype: float
         """
-        # if n >= 5000:
-        #     return 1
-        # def solve(a,b):
-
-        #     if a<=0 and b<=0:
-        #         return 0.5
-        #     if a<=0:
-        #         return 1.0
-        #     if b<=0:
-        #         return 0.0
-        #     return 0.25*(solve(a-100,b)+solve(a-75,b-25)+solve(a-50,b-50)+solve(a-25,b-75))
-    
-        # return solve(n,n)
         memo = {}
-        
         def dfs(a, b):
             if a <= 0 and b <= 0:
                 return 0.5
