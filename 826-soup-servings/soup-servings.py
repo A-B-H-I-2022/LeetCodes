353bf076c1cba7778e714bad8cd0d91150c7a23c
ABHIJITH P SHABU <96898946+A-B-H-I-2022@users.noreply.github.com>
2023-09-07 11:14:22 +0530

    Time: 22 ms (44.96%) | Memory: 15.5 MB (12.78%) - LeetSync

--- 826-soup-servings/soup-servings.py
+++ 826-soup-servings/soup-servings.py
@@ -4,26 +4,19 @@ class Solution(object):
         :type n: int
         :rtype: float
         """
-        if n >= 5000:
+        if n >= 4800:
             return 1
         memo = {}
         def dfs(a, b):
+            if (a, b) in memo:
+                return memo[(a, b)]
             if a <= 0 and b <= 0:
                 return 0.5
             if b <= 0:
                 return 0
             if a <= 0:
                 return 1
-
-            if (a, b) in memo:
-                return memo[(a, b)]
-
-            prob_a1 = dfs(a - 100, b)
-            prob_a2 = dfs(a - 75, b - 25)
-            prob_a3 = dfs(a - 50, b - 50)
-            prob_a4 = dfs(a - 25, b - 75)
-
-            memo[(a, b)] = 0.25 * (prob_a1 + prob_a2 + prob_a3 + prob_a4)
+            memo[(a, b)] = 0.25 * (dfs(a - 100, b)+dfs(a - 75, b - 25)+dfs(a - 50, b - 50)+dfs(a - 25, b - 75))
             return memo[(a, b)]
 
         return dfs(n, n)
