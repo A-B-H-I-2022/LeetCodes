15f17f2e39b0b7ca57eafe1b1892abb4a71385cf
ABHIJITH P SHABU <96898946+A-B-H-I-2022@users.noreply.github.com>
2023-09-07 19:06:45 +0530

    Time: 19 ms (56.98%) | Memory: 13.1 MB (89.64%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -4,8 +4,16 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
+        n=len(matrix)
         matrix.reverse()
-        for i in range(len(matrix)):
-            for j in range(i):
-                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        for i in range(n):
+            for j in range(i+1,n):
+                c=matrix[i][j]
+                matrix[i][j]=matrix[j][i]
+                matrix[j][i]=c
+        
+        # matrix.reverse()
+        # for i in range(len(matrix)):
+        #     for j in range(i):
+        #         matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
         
