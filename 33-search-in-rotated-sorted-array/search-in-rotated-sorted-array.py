fb43804767261f9fdb1c3b9d7053eba09b1e00cb
ABHIJITH P SHABU <96898946+A-B-H-I-2022@users.noreply.github.com>
2023-09-08 12:29:17 +0530

    Time: 20 ms (80.07%) | Memory: 13.6 MB (48.86%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -5,30 +5,50 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        length = len(nums)
-        l,r = 0,length - 1
-        while l <= r:
-            mid = l + (r - l) // 2
-            if nums[mid] > nums[-1]:
-                l = mid + 1
-            else :
-                r = mid - 1
-        pivot = l
-        def bs(left,right,target):
-            l,r = left,right
-            while l <= r:
-                mid = l + (r - l) // 2
-                if nums[mid] == target:
-                    return mid
-                elif nums[mid] > target:
-                    r = mid - 1
-                else :
-                    l = mid + 1
-            return -1
+        # length = len(nums)
+        # l,r = 0,length - 1
+        # while l <= r:
+        #     mid = l + (r - l) // 2
+        #     if nums[mid] > nums[-1]:
+        #         l = mid + 1
+        #     else :
+        #         r = mid - 1
+        # pivot = l
+        # def bs(left,right,target):
+        #     l,r = left,right
+        #     while l <= r:
+        #         mid = l + (r - l) // 2
+        #         if nums[mid] == target:
+        #             return mid
+        #         elif nums[mid] > target:
+        #             r = mid - 1
+        #         else :
+        #             l = mid + 1
+        #     return -1
 
-        n1 = bs(0,pivot-1,target)
-        n2 = bs(pivot,length-1,target)
-        if n1 != -1:
-            return n1
-        else:
-            return n2
+        # n1 = bs(0,pivot-1,target)
+        # n2 = bs(pivot,length-1,target)
+        # if n1 != -1:
+        #     return n1
+        # else:
+        #     return n2
+                                  #doubt
+
+        l, r = 0, len(nums) -1
+
+        while l <= r:
+            mid = (l+r)//2
+            if target == nums[mid]:
+                return mid
+            if nums[l] <= nums[mid]:
+                if target > nums[mid] or target < nums[l]:
+
+                    l = mid+1
+                else:
+                    r = mid - 1
+            else:
+                if target < nums[mid] or target > nums[r]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+        return -1
