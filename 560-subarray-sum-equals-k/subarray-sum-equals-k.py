89847fb48918df92c085cfab09807ebcbffd12a5
ABHIJITH P SHABU <96898946+A-B-H-I-2022@users.noreply.github.com>
2023-09-09 16:07:22 +0530

    Time: 224 ms (67.40%) | Memory: 15.6 MB (99.33%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -5,40 +5,17 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        # count = 0
-        # dic = {0 : 1}
-        # summ = 0
-        # for i in nums:
-        #     summ += i
-        #     if summ - k in dic:
-        #         count += dic[summ-k]
-        #     if summ in dic:
-        #         dic[summ] += 1
-        #     else:
-        #         dic[summ] = 1
-        # return count
-
-        anscount = 0
-        prefixsum = 0
-        
-        d ={0:1}
-        
+        count = 0
+        dic = {0 : 1}
+        summ = 0
         for i in nums:
-            
-            prefixsum = prefixsum + i
-            
-            if prefixsum - k in d:
-                anscount = anscount + d[prefixsum - k]
-                #print(d)
-                #print(k)
-                #print(prefixsum)
-                #print(prefixsum-k)
-                #print(d[prefixsum-k])
-                #print(d[0])
-            if prefixsum not in d:
-                d[prefixsum] = 1
+            summ += i
+            if summ - k in dic:
+                count += dic[summ-k]
+            if summ not in dic:
+                dic[summ] = 1
             else:
-                d[prefixsum] = d[prefixsum] + 1                                                
-        
-        return anscount
-        
+                dic[summ] += 1
+        return count
+
+    
