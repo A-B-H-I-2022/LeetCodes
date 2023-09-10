002a5593a823fa3fe9296a450721769e1c47bfe4
ABHIJITH P SHABU <96898946+A-B-H-I-2022@users.noreply.github.com>
2023-09-10 20:06:35 +0530

    Time: 28 ms (73.59%) | Memory: 13.9 MB (71.46%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -4,21 +4,33 @@ class Solution(object):
         :type nums: List[int]
         :type target: int
         :rtype: int
-        """
-        left = 0
-        right = len(nums) - 1 
-        while left <= right:
-            flag = 0
-            mid =  (left + right) // 2
-            if nums[mid] == target:
-                return mid
-                flag = 1
+        # """
+        # left = 0
+        # right = len(nums) - 1 
+        # while left <= right:
+        #     flag = 0
+        #     mid =  (left + right) // 2
+        #     if nums[mid] == target:
+        #         return mid
+        #         flag = 1
+        #     elif nums[mid] > target:
+        #         right = mid - 1
+        #     elif nums[mid] < target:
+        #         left = mid + 1
+        # if not flag:
+        #     if nums[mid] > target:
+        #         return mid
+        #     else:
+        #         return mid + 1
+
+        low = 0
+        high = len(nums) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            if nums[mid] < target:
+                low = mid + 1
             elif nums[mid] > target:
-                right = mid - 1
-            elif nums[mid] < target:
-                left = mid + 1
-        if not flag:
-            if nums[mid] > target:
-                return mid
+                high = mid - 1
             else:
-                return mid + 1
+                return mid
+        return high + 1
