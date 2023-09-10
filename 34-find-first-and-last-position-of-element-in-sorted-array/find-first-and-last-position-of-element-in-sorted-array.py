e7c2060d98d429363a8f1e21ffb88578e119779a
ABHIJITH P SHABU <96898946+A-B-H-I-2022@users.noreply.github.com>
2023-09-10 20:00:30 +0530

    Time: 53 ms (87.48%) | Memory: 14.4 MB (76.94%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -6,20 +6,42 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        def search(x):
-            lo, hi = 0, len(nums)           
-            while lo < hi:
-                mid = (lo + hi) // 2
-                if nums[mid] < x:
-                    lo = mid+1
-                else:
-                    hi = mid                    
-            return lo
+        # def search(x):
+        #     lo, hi = 0, len(nums)           
+        #     while lo < hi:
+        #         mid = (lo + hi) // 2
+        #         if nums[mid] < x:
+        #             lo = mid+1
+        #         else:
+        #             hi = mid                    
+        #     return lo
         
-        lo = search(target)
-        hi = search(target+1)-1
+        # lo = search(target)
+        # hi = search(target+1)-1
         
-        if lo <= hi:
-            return [lo, hi]
+        # if lo <= hi:
+        #     return [lo, hi]
                 
-        return [-1, -1]
+        # return [-1, -1]
+
+        def find(last = True):
+            l = 0
+            r = len(nums)-1
+            ans = -1
+            while l <=r:
+                m = (l + r)//2
+                if nums[m] < target:
+                    l = m+1
+                elif nums[m] > target:
+                    r = m-1
+                else:
+                    ans = m
+                    if last:
+                        l = m +1
+                    else:
+                        r = m -1
+            
+            return ans
+        
+        
+        return [find(False),find(True)]
