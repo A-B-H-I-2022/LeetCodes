3730f262e58ed3f8d586d12ff6af5ffc122effa2
ABHIJITH P SHABU <96898946+A-B-H-I-2022@users.noreply.github.com>
2023-09-10 20:21:33 +0530

    Time: 59 ms (83.37%) | Memory: 13.5 MB (28.34%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -11,10 +11,10 @@ class Solution(object):
             for j in range(9):
                 if board[i][j] == ".":
                     continue
-                elif(board[i][j] in r[i] or board[i][j] in c[j] or board[i][j] in part[(i//3,j//3)]):
+                if(board[i][j] in r[i] or board[i][j] in c[j] or board[i][j] in part[(i//3,j//3)]):
                     return False
-                else:
-                    r[i].add(board[i][j])
-                    c[j].add(board[i][j])
-                    part[(i//3,j//3)].add(board[i][j])
+                # else:
+                r[i].add(board[i][j])
+                c[j].add(board[i][j])
+                part[(i//3,j//3)].add(board[i][j])
         return True
