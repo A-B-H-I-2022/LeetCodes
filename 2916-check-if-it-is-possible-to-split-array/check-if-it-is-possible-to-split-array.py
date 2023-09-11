1a52baf3d1c11049677583b9a16ceb94cc55753a
ABHIJITH P SHABU <96898946+A-B-H-I-2022@users.noreply.github.com>
2023-09-11 22:03:01 +0530

    Time: 24 ms (91.13%) | Memory: 13.3 MB (35.49%) - LeetSync

--- 2916-check-if-it-is-possible-to-split-array/check-if-it-is-possible-to-split-array.py
+++ 2916-check-if-it-is-possible-to-split-array/check-if-it-is-possible-to-split-array.py
@@ -5,10 +5,18 @@ class Solution(object):
         :type m: int
         :rtype: bool
         """
-        if len(nums) < 3:
+        # if len(nums) < 3:
+        #     return True
+        # for i in range(len(nums)-1):
+        #     if nums[i] + nums[i+1] >= m:
+        #         return True
+        # return False
+
+        if len(nums) <= 2:
             return True
-        flag = 0
-        for i in range(len(nums)-1):
-            if nums[i] + nums[i+1] >= m:
+        
+        for i in range(1, len(nums)):
+            if nums[i] + nums[i-1] >= m:
                 return True
         return False
+            
