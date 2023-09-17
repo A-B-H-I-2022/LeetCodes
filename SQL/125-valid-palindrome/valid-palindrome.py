71edeb901525c80874ed40f103288af1b34a8b34
ABHIJITH P SHABU <96898946+A-B-H-I-2022@users.noreply.github.com>
2023-09-17 21:49:13 +0530

    Time: 302 ms (16.21%) | Memory: 14.8 MB (50.66%) - LeetSync

--- SQL/125-valid-palindrome/valid-palindrome.py
+++ SQL/125-valid-palindrome/valid-palindrome.py
@@ -4,17 +4,24 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        flag = 1
-        B = ""
-        if len(s) == 0:
-            return 0
-        for i in range(len(s)):
-            if s[i].isalnum():
-                B += s[i].lower()
-        for i in range(len(B)//2):
-            if (B[i] != B[-i-1]):
-                return 0
-            else :
-                flag = 1
-        if flag == 1:
-            return 1
+        s1 = ''
+        for c in s.lower():
+            if c.isalnum():
+                s1 += c
+
+        return True if s1==s1[::-1] else False
+
+        # flag = 1
+        # B = ""
+        # if len(s) == 0:
+        #     return 0
+        # for i in range(len(s)):
+        #     if s[i].isalnum():
+        #         B += s[i].lower()
+        # for i in range(len(B)//2):
+        #     if (B[i] != B[-i-1]):
+        #         return 0
+        #     else :
+        #         flag = 1
+        # if flag == 1:
+        #     return 1
