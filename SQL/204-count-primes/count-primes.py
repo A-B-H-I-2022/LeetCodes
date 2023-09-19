2e944a9e84944b53c7844f1bb0da05483f6d4524
ABHIJITH P SHABU <96898946+A-B-H-I-2022@users.noreply.github.com>
2023-09-19 18:50:30 +0530

    Time: 754 ms (96.82%) | Memory: 90.8 MB (92.96%) - LeetSync

--- SQL/204-count-primes/count-primes.py
+++ SQL/204-count-primes/count-primes.py
@@ -4,13 +4,17 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        seen, ans = [0] * n, 0
-        for num in range(2, n):
-            if seen[num]: continue
-            ans += 1
-            seen[num*num:n:num] = [1] * ((n - 1) // num - num + 1)
-        return ans
+        if n <= 2:
+            return 0
 
+        is_prime = [True] * n
+        is_prime[0] = is_prime[1] = False
+
+        for i in range(2, int(n ** 0.5) + 1):
+            if is_prime[i]:
+                is_prime[i*i:n:i] = [False] * len(is_prime[i*i:n:i])
+
+        return sum(is_prime)
 
         # count = 1
         # flag = 0
