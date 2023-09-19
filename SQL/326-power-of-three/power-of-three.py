91411fb4563ad78a127e4214a6b6b66d187e2485
ABHIJITH P SHABU <96898946+A-B-H-I-2022@users.noreply.github.com>
2023-09-19 15:12:02 +0530

    Time: 36 ms (98.22%) | Memory: 13.2 MB (82.51%) - LeetSync

--- SQL/326-power-of-three/power-of-three.py
+++ SQL/326-power-of-three/power-of-three.py
@@ -4,17 +4,18 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        x = 1
-        if n <= 0 :
-            return False
-        elif n == 1:
-            return True
-        else:
-            while x < n:
-                x = x*3
-                if x == n:
-                    return True
-            return False
+        return (n>0) and 1162261467%n == 0
+        # x = 1
+        # if n <= 0 :
+        #     return False
+        # elif n == 1:
+        #     return True
+        # else:
+        #     while x < n:
+        #         x = x*3
+        #         if x == n:
+        #             return True
+        #     return False
                 
 
         
