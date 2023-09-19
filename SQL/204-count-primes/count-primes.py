5a9d8c4dd4f1643005e17c2e6844c48b632027ac
ABHIJITH P SHABU <96898946+A-B-H-I-2022@users.noreply.github.com>
2023-09-19 19:29:52 +0530

    Time: 755 ms (96.82%) | Memory: 91 MB (90.95%) - LeetSync

--- SQL/204-count-primes/count-primes.py
+++ SQL/204-count-primes/count-primes.py
@@ -7,14 +7,14 @@ class Solution(object):
         if n <= 2:
             return 0
 
-        is_prime = [True] * n
-        is_prime[0] = is_prime[1] = False
+        dp = [True] * n
+        dp[0] = dp[1] = False
 
         for i in range(2, int(n ** 0.5) + 1):
-            if is_prime[i]:
-                is_prime[i*i:n:i] = [False] * len(is_prime[i*i:n:i])
+            if dp[i]:
+                dp[i*i:n:i] = [False] * len(dp[i*i:n:i])
 
-        return sum(is_prime)
+        return sum(dp)
 
         # count = 1
         # flag = 0
