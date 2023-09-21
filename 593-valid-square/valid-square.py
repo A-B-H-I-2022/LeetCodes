6d367e8923350c06c0118727e2d2e1de0e8dd7d6
ABHIJITH P SHABU <96898946+A-B-H-I-2022@users.noreply.github.com>
2023-09-21 13:16:57 +0530

    Time: 15 ms (67.86%) | Memory: 13.4 MB (10.71%) - LeetSync

---
+++ 593-valid-square/valid-square.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def validSquare(self, p1, p2, p3, p4):
+        """
+        :type p1: List[int]
+        :type p2: List[int]
+        :type p3: List[int]
+        :type p4: List[int]
+        :rtype: bool
+        """
+        if p1 == p2 == p3 == p4:
+            return False
+        def square(a,b):
+            return (a[0] - b[0])**2 + (a[1] - b[1])**2
+        ans = [square(p1,p2),square(p1,p3),square(p1,p4),square(p2,p3),square(p2,p4),square(p3,p4)]
+        ans.sort()
+        if ans[0]==ans[1]==ans[2]==ans[3]:
+            if ans[4]==ans[5]:
+                return True
+            return False
+        
+        
