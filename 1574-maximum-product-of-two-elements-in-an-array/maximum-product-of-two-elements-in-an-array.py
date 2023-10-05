d9a1562586cc3cb3605c4f9e5dfca9092ab7174d
ABHIJITH P SHABU <96898946+A-B-H-I-2022@users.noreply.github.com>
2023-10-05 17:29:16 +0530

    Time: 26 ms (87.18%) | Memory: 13.4 MB (43.82%) - LeetSync

--- 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.py
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.py
@@ -4,11 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+    # a,b = 0,0
+    # heapq.heapify(nums)
+    # while len(nums) > 2:
+    #     heapq.heappop(nums)
+    # a = heapq.heappop(nums)
+    # b = heapq.heappop(nums)
+    # return (a-1)*(b-1)
         a,b = 0,0
-        heapq.heapify(nums)
-        while len(nums) > 2:
-            heapq.heappop(nums)
-        a = heapq.heappop(nums)
-        b = heapq.heappop(nums)
+        for i in nums:
+            if i > a:
+                b = a
+                a = i
+            elif i > b:
+                b = i
         return (a-1)*(b-1)
-        
