df499a00af73a631b3e3c0e3359acc8d7a626964
ABHIJITH P SHABU <96898946+A-B-H-I-2022@users.noreply.github.com>
2023-12-26 15:43:26 +0530

    Time: 15 ms (62.56%) | Memory: 13.4 MB (43.37%) - LeetSync

--- 171-excel-sheet-column-number/excel-sheet-column-number.py
+++ 171-excel-sheet-column-number/excel-sheet-column-number.py
@@ -4,13 +4,20 @@ class Solution(object):
         :type columnTitle: str
         :rtype: int
         """
+        # ans = 0
+        # num = [i for i in range(1,27)]
+        # lis = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+        # map = dict(zip(lis,num))
+        # print(map)
+        # for j in columnTitle:
+        #     ans = ans*26 + map[j]
+        # return ans
+
+        l = len(columnTitle) - 1
         ans = 0
-        num = [i for i in range(1,27)]
-        lis = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-        map = dict(zip(lis,num))
-        print(map)
-        for j in columnTitle:
-            ans = ans*26 + map[j]
+        for s in columnTitle:
+            ans += ((ord(s) - 64)*(26**l))
+            l = l-1
         return ans
         
         
