648c2a23ef72e6dd1954ea603b592d62aa3fabcf
ABHIJITH P SHABU <96898946+A-B-H-I-2022@users.noreply.github.com>
2024-07-08 09:35:09 +0530

    Time: 23 ms (58.23%) | Memory: 11.8 MB (31.69%) - LeetSync

---
+++ 179-largest-number/largest-number.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def largestNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: str
+        """
+        for i,n in enumerate(nums):
+            nums[i] = str(n)
+
+        def larger(n1,n2):
+            if n1+n2 > n2+n1:
+                return -1
+            else:
+                return 1
+        nums = sorted(nums, key = cmp_to_key(larger))
+        return str(int("".join(nums)))
+        
