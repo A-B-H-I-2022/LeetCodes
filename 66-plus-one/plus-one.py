8325320c1c6cf45506a0e3fb2ed693498936620d
ABHIJITH P SHABU <96898946+A-B-H-I-2022@users.noreply.github.com>
2024-12-27 09:01:15 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (11.15%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -4,18 +4,27 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-        given = 0
-        l = len(digits)-1
-        for i in digits:
-            given += i*(10**l)
-            l -= 1 
-        given += 1
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] = digits[i] + 1
+                return digits
+        return [1] + digits
 
-        ans = []
+
+        # given = 0
+        # l = len(digits)-1
+        # for i in digits:
+        #     given += i*(10**l)
+        #     l -= 1 
+        # given += 1
+
+        # ans = []
         
-        while given > 0:
-            a = given % 10
-            given = given // 10
-            ans.append(a)
+        # while given > 0:
+        #     a = given % 10
+        #     given = given // 10
+        #     ans.append(a)
         
-        return ans[::-1]
+        # return ans[::-1]
